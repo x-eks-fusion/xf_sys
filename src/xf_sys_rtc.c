a47943b2b07544e343fce9fc906daafd918e9177
cangyu <sky.kirto@qq.com>
2025-02-13 18:27:47 +0800

    🐞 fix: 修改了笔误部分

--- src/xf_sys_rtc.c
+++ src/xf_sys_rtc.c
@@ -1,12 +1,12 @@
 /**
  * @file xf_sys_rtc.c
  * @author cangyu (sky.kirto@qq.com)
- * @brief 
+ * @brief
  * @version 0.1
  * @date 2025-02-13
- * 
+ *
  * @copyright Copyright (c) 2025, CorAL. All rights reserved.
- * 
+ *
  */
 
 /* ==================== [Includes] ========================================== */
@@ -27,13 +27,25 @@
 
 #if XF_SYS_RTC_WEAK_IS_ENABLE
 
-__weak xf_err_t xf_sys_rtc_init(void);
+__weak xf_err_t xf_sys_rtc_init(void)
+{
 
-__weak xf_err_t xf_sys_rtc_deinit(void);
+}
 
-__weak uint64_t xf_sys_get_rtc_count(void);
+__weak xf_err_t xf_sys_rtc_deinit(void)
+{
 
-__weak xf_err_t xf_sys_set_rtc_count(uint64_t count);
+}
+
+__weak uint64_t xf_sys_get_rtc_count(void)
+{
+
+}
+
+__weak xf_err_t xf_sys_set_rtc_count(uint64_t count)
+{
+
+}
 
 #endif
 
